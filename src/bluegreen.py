9ded7d910c1b2d1c60c57a88887cc68607c01fdc
Emerson Macedo <emerleite@gmail.com>
2014-10-06 17:51:50 -0300

    Refactor to create BlueGreen class and added some tests. Closes #4

--- src/bluegreen.py
+++ src/bluegreen.py
@@ -10,106 +10,113 @@ import argparse
 import time
 from urlparse import urlparse
 
-def get_cname(app):
-  headers = {"Authorization" : "bearer " + token}
-  conn = httplib.HTTPConnection(target)
-  conn.request("GET", "/apps/" + app, "", headers)
-  response = conn.getresponse()
-  data = json.loads(response.read())
-  if len(data.get("cname")) == 0:
-    return None
-  return data.get("cname")
+class BlueGreen:
+  def __init__(self, token, target, config):
+    self.token = token
+    self.target = target
+    self.app_name = config['name']
+    self.production_instances = config['units']
 
-def remove_cname(app, cname):
-  headers = {"Authorization" : "bearer " + token}
-  conn = httplib.HTTPConnection(target)
-  conn.request("DELETE", "/apps/" + app + '/cname', '{"cname": ' + json.dumps(cname) + '}', headers)
-  response = conn.getresponse()
-  if response.status != 200:
-    return False
-  return True
+  def get_cname(self, app):
+    headers = {"Authorization" : "bearer " + self.token}
+    conn = httplib.HTTPConnection(self.target)
+    conn.request("GET", "/apps/" + app, "", headers)
+    response = conn.getresponse()
+    data = json.loads(response.read())
+    if len(data.get("cname")) == 0:
+      return None
+    return data.get("cname")
 
-def set_cname(app, cname):
-  headers = {"Content-Type" : "application/json", "Authorization" : "bearer " + token}
-  conn = httplib.HTTPConnection(target)
-  conn.request("POST", "/apps/" + app + '/cname', '{"cname": ' + json.dumps(cname) + '}', headers)
-  response = conn.getresponse()
-  if response.status != 200:
-    return False
-  return True
+  def remove_cname(self, app, cname):
+    headers = {"Authorization" : "bearer " + self.token}
+    conn = httplib.HTTPConnection(self.target)
+    conn.request("DELETE", "/apps/" + app + '/cname', '{"cname": ' + json.dumps(cname) + '}', headers)
+    response = conn.getresponse()
+    if response.status != 200:
+      return False
+    return True
 
-def deploy_pre(app, tag):
-  print """
-Pre deploying tag:%s to %s ...
-  """ % (tag, app)
+  def set_cname(self, app, cname):
+    headers = {"Content-Type" : "application/json", "Authorization" : "bearer " + self.token}
+    conn = httplib.HTTPConnection(self.target)
+    conn.request("POST", "/apps/" + app + '/cname', '{"cname": ' + json.dumps(cname) + '}', headers)
+    response = conn.getresponse()
+    if response.status != 200:
+      return False
+    return True
 
-  process = subprocess.Popen(['git', 'push', app, "%s:master" % tag], stdout=subprocess.PIPE)
-  for line in iter(process.stdout.readline, ''):
-    sys.stdout.write(line)
+  def total_units(self, app):
+    headers = {"Authorization" : "bearer " + self.token}
+    conn = httplib.HTTPConnection(self.target)
+    conn.request("GET", "/apps/" + app, "", headers)
+    response = conn.getresponse()
+    data = json.loads(response.read())
+    return len(data.get('units'))
 
-def deploy_swap(apps, cname):
-  print """
-Changing live application to %s ...""" % apps[1]
-
-  if not add_units(apps[1], production_instances):
-    sys.exit()
-
-  if not remove_cname(apps[0], cname):
-    print "Error removing cname of %s. Aborting..." % apps[0]
-    remove_units(apps[1])
-    sys.exit()
-
-  if set_cname(apps[1], cname):
-    remove_units(apps[0])
-
+  def remove_units(self, app):
+    units = str(self.total_units(app)-1)
     print """
-Application %s is live at %s ...
-    """ % (apps[1], ','.join(cname))
+  Removing %s units from %s ...""" % (units, app)
 
-  else:
-    print "Error adding cname of %s. Aborting..." % apps[1]
-    set_cname(apps[0], cname)
-    remove_units(apps[1])
+    headers = {"Authorization" : "bearer " + self.token}
+    conn = httplib.HTTPConnection(self.target)
+    conn.request("DELETE", "/apps/" + app + '/units', units, headers)
+    response = conn.getresponse()
+    if response.status != 200:
+      print "Error removing units from %s. You'll need to remove manually." % app
+      return False
 
-def add_units(app, current_units):
-  units = str(int(current_units) - total_units(app))
-  print """
-Adding %s units to %s ...""" % (units, app)
+    while (self.total_units(app) > 1):
+      print "Waiting for %s units to go down..." % app
+      time.sleep(1)
+    return True
 
-  headers = {"Authorization" : "bearer " + token}
-  conn = httplib.HTTPConnection(target)
-  conn.request("PUT", "/apps/" + app + '/units', units, headers)
-  response = conn.getresponse()
-  if response.status != 200:
-    print "Error adding units to %s. Aborting..." % app
-    return False
-  return True
+  def add_units(self, app, current_units):
+    units = str(int(current_units) - self.total_units(app))
+    print """
+  Adding %s units to %s ...""" % (units, app)
 
-def remove_units(app):
-  units = str(total_units(app)-1)
-  print """
-Removing %s units from %s ...""" % (units, app)
+    headers = {"Authorization" : "bearer " + self.token}
+    conn = httplib.HTTPConnection(self.target)
+    conn.request("PUT", "/apps/" + app + '/units', units, headers)
+    response = conn.getresponse()
+    if response.status != 200:
+      print "Error adding units to %s. Aborting..." % app
+      return False
+    return True
 
-  headers = {"Authorization" : "bearer " + token}
-  conn = httplib.HTTPConnection(target)
-  conn.request("DELETE", "/apps/" + app + '/units', units, headers)
-  response = conn.getresponse()
-  if response.status != 200:
-    print "Error removing units from %s. You'll need to remove manually." % app
-    return False
+  def deploy_pre(self, app, tag):
+    print """
+  Pre deploying tag:%s to %s ...
+    """ % (tag, app)
 
-  while (total_units(app) > 1):
-    print "Waiting for %s units to go down..." % app
-    time.sleep(1)
-  return True
+    process = subprocess.Popen(['git', 'push', app, "%s:master" % tag], stdout=subprocess.PIPE)
+    for line in iter(process.stdout.readline, ''):
+      sys.stdout.write(line)
 
-def total_units(app):
-  headers = {"Authorization" : "bearer " + token}
-  conn = httplib.HTTPConnection(target)
-  conn.request("GET", "/apps/" + app, "", headers)
-  response = conn.getresponse()
-  data = json.loads(response.read())
-  return len(data.get('units'))
+  def deploy_swap(self, apps, cname):
+    print """
+  Changing live application to %s ...""" % apps[1]
+
+    if not self.add_units(apps[1], self.production_instances):
+      sys.exit()
+
+    if not self.remove_cname(apps[0], cname):
+      print "Error removing cname of %s. Aborting..." % apps[0]
+      self.remove_units(apps[1])
+      sys.exit()
+
+    if self.set_cname(apps[1], cname):
+      self.remove_units(apps[0])
+
+      print """
+  Application %s is live at %s ...
+      """ % (apps[1], ','.join(cname))
+
+    else:
+      print "Error adding cname of %s. Aborting..." % apps[1]
+      self.set_cname(apps[0], cname)
+      self.remove_units(apps[1])
 
 if __name__ == "__main__":
   #Initialization
@@ -135,8 +142,10 @@ if __name__ == "__main__":
   blue = "%s-blue" % app_name
   green = "%s-green" % app_name
 
+  bluegreen = BlueGreen(token, target, {'name' : app_name, 'units' : production_instances})
+
   apps = [blue, green]
-  cnames = [get_cname(green), get_cname(blue)]
+  cnames = [bluegreen.get_cname(green), bluegreen.get_cname(blue)]
 
   #reverse if first is not None
   if cnames[0] is not None:
@@ -147,6 +156,6 @@ if __name__ == "__main__":
   pre = apps[1]
 
   if args.action == 'pre':
-    deploy_pre(pre, args.tag)
+    bluegreen.deploy_pre(pre, args.tag)
   elif args.action == 'swap':
-    deploy_swap(apps, cname)
+    bluegreen.deploy_swap(apps, cname)
---
+++ test/bluegreen_teste.py
@@ -0,0 +1,121 @@
+import unittest
+from mock import MagicMock
+from mock import Mock
+import httpretty
+from bluegreen import BlueGreen
+
+class TestBlueGreen(unittest.TestCase):
+
+  def setUp(self):
+    config = {'name': 'test-app', 'units' : '2'}
+
+    self.bg = BlueGreen('token', 'tsuru.globoi.com', config)
+    self.cnames = [u'cname1', u'cname2']
+
+  @httpretty.activate
+  def test_get_cname_returns_a_list_when_present(self):
+    httpretty.register_uri(httpretty.GET, 'http://tsuru.globoi.com/apps/xpto',
+                           body='{"cname":["cname1", "cname2"]}')
+
+    self.assertEqual(self.bg.get_cname('xpto'), self.cnames)
+
+  @httpretty.activate
+  def test_get_cname_returns_none_when_empty(self):
+    httpretty.register_uri(httpretty.GET, 'http://tsuru.globoi.com/apps/xpto',
+                           body='{"cname":[]}')
+
+    self.assertIsNone(self.bg.get_cname('xpto'))
+
+  @httpretty.activate
+  def test_remove_cname_return_true_when_can_remove(self):
+    httpretty.register_uri(httpretty.DELETE, 'http://tsuru.globoi.com/apps/xpto/cname',
+                           data='{"cname":["cname1", "cname2"]}',
+                           status=200)
+
+    self.assertTrue(self.bg.remove_cname('xpto', self.cnames))
+
+  @httpretty.activate
+  def test_remove_cname_return_false_when_cant_remove(self):
+    httpretty.register_uri(httpretty.DELETE, 'http://tsuru.globoi.com/apps/xpto/cname',
+                           data='{"cname":["cname1", "cname2"]}',
+                           status=500)
+
+    self.assertFalse(self.bg.remove_cname('xpto', self.cnames))
+
+  @httpretty.activate
+  def test_set_cname_return_true_when_can_set(self):
+    httpretty.register_uri(httpretty.POST, 'http://tsuru.globoi.com/apps/xpto/cname',
+                           data='{"cname":["cname1", "cname2"]}',
+                           status=200)
+
+    self.assertTrue(self.bg.set_cname('xpto', self.cnames))
+
+  @httpretty.activate
+  def test_set_cname_return_false_when_cant_set(self):
+    httpretty.register_uri(httpretty.POST, 'http://tsuru.globoi.com/apps/xpto/cname',
+                           data='{"cname":["cname1", "cname2"]}',
+                           status=500)
+
+    self.assertFalse(self.bg.set_cname('xpto', self.cnames))
+
+  @httpretty.activate
+  def test_total_units_zero_when_empty(self):
+    httpretty.register_uri(httpretty.GET, 'http://tsuru.globoi.com/apps/xpto',
+                           body='{"units":[]}',
+                           status=500)
+
+    self.assertEqual(self.bg.total_units('xpto'), 0)
+
+  @httpretty.activate
+  def test_total_units_gt_zero_when_not_empty(self):
+    httpretty.register_uri(httpretty.GET, 'http://tsuru.globoi.com/apps/xpto',
+                           body='{"units":["unit1"]}',
+                           status=500)
+
+    self.assertGreater(self.bg.total_units('xpto'), 0)
+
+  @httpretty.activate
+  def test_remove_units_should_return_true_when_removes(self):
+    def side_effect(*args):
+      def second_call(*args):
+        return 1
+      self.bg.total_units.side_effect = second_call
+      return 2
+
+    self.bg.total_units = Mock(side_effect=side_effect)
+
+    httpretty.register_uri(httpretty.DELETE, 'http://tsuru.globoi.com/apps/xpto/units',
+                           data='1',
+                           status=200)
+
+    self.assertTrue(self.bg.remove_units('xpto'))
+
+  @httpretty.activate
+  def test_remove_units_should_return_false_when_not_removes(self):
+    self.bg.total_units = MagicMock(return_value=2)
+
+    httpretty.register_uri(httpretty.DELETE, 'http://tsuru.globoi.com/apps/xpto/units',
+                           data='1',
+                           status=500)
+
+    self.assertFalse(self.bg.remove_units('xpto'))
+
+  @httpretty.activate
+  def test_add_units_should_return_true_when_adds(self):
+    self.bg.total_units = MagicMock(return_value=1)
+
+    httpretty.register_uri(httpretty.PUT, 'http://tsuru.globoi.com/apps/xpto/units',
+                           data='1',
+                           status=200)
+
+    self.assertTrue(self.bg.add_units('xpto', 2))
+
+  @httpretty.activate
+  def test_add_units_should_return_false_when_adds(self):
+    self.bg.total_units = MagicMock(return_value=1)
+
+    httpretty.register_uri(httpretty.PUT, 'http://tsuru.globoi.com/apps/xpto/units',
+                           data='1',
+                           status=500)
+
+    self.assertFalse(self.bg.add_units('xpto', 2))
